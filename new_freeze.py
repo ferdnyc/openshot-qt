8d20e6ad60083a70f8e28f161e5f176aab0e2b5f
FeRD (Frank Dana) <ferdnyc@gmail.com>
2020-08-08 00:33:30 -0400

    Packaging

---
+++ new_freeze.py
@@ -0,0 +1,278 @@
+"""
+ @file
+ @brief cx_Freeze script to build OpenShot package with dependencies (for Mac and Windows)
+ @author Jonathan Thomas <jonathan@openshot.org>
+
+ @section LICENSE
+
+ Copyright (c) 2008-2016 OpenShot Studios, LLC
+ (http://www.openshotstudios.com). This file is part of
+ OpenShot Video Editor (http://www.openshot.org), an open-source project
+ dedicated to delivering high quality video editing and animation solutions
+ to the world.
+
+ OpenShot Video Editor is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenShot Video Editor is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenShot Library.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+# Syntax to build redistributable package:  python3 freeze.py build
+#
+# Troubleshooting: If you encounter an error while attempting to freeze
+# the PyQt5/uic/port_v2, remove the __init__.py in that folder. And if
+# you are manually compiling PyQt5 on Windows, remove the -strip line
+# from the Makefile. On Mac, just delete the port_v2 folder. Also, you
+# might need to remove the QtTest.so from /usr/local/lib/python3.3/site-packages/PyQt5,
+# if you get errors while freezing.
+#
+# Mac Syntax to Build App Bundle:
+# 1) python3 freeze.py bdist_mac --qt-menu-nib="/usr/local/Cellar/qt5/5.4.2/plugins/platforms/" --iconfile=installer/openshot.icns --custom-info-plist=installer/Info.plist --bundle-name="OpenShot Video Editor"
+# 2) change Contents/Info.plist to use launch-mac.sh as the Executable name
+# 3) manually fix rsvg executable:
+#    sudo dylibbundler -od -of -b -x ~/apps/rsvg/rsvg-convert -d ./rsvg-libs/ -p @executable_path/rsvg-libs/
+# 4) Code sign and create the DMG (disk image)
+#    a) cd ~/apps/openshot-qt-git/
+#    b) bash installer/build-mac-dmg.sh
+#
+# Windows Syntax to Build MSI Installer
+# NOTE: Python3.5 requires custom build of cx_Freeze (https://github.com/sekrause/cx_Freeze-wheels). Download, python setup.py build, python setup.py install
+# 1) python3 freeze.py bdist_msi
+# NOTE: Requires a tweak to cx_freeze: http://stackoverflow.com/questions/24195311/how-to-set-shortcut-working-directory-in-cx-freeze-msi-bundle
+# 2) Sign MSI with private code signing key (optional)
+#  NOTE: Install Windows 10 SDK first
+#  signtool sign /v /f OSStudiosSPC.pfx "OpenShot Video Editor-2.0.0-win32.msi"
+
+import inspect
+import os
+import sys
+import fnmatch
+import setuptools
+import cx_Freeze
+from PyQt5.QtCore import QLibraryInfo
+import shutil
+
+
+# Set '${ARCHLIB}' envvar to override system library path
+ARCHLIB = os.getenv('ARCHLIB', "/usr/lib/x86_64-linux-gnu/")
+if not ARCHLIB.endswith('/'):
+    ARCHLIB += '/'
+
+# Packages to include
+python_packages = [
+    "os",
+    "sys",
+    "PyQt5",
+    "time",
+    "uuid",
+    "shutil",
+    "threading",
+    "subprocess",
+    "re",
+    "math",
+    "xml",
+    "logging",
+    "urllib",
+    "requests",
+    "zmq",
+    "webbrowser",
+    "json",
+    "libopenshot",
+]
+
+# Determine absolute PATH of OpenShot repo
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.insert(0, '.')
+import openshot_qt
+from openshot_qt.classes import info
+from openshot_qt.classes.logger import log
+
+log.info("Execution path: %s" % PATH)
+
+# Find files matching patterns
+def find_files(directory, patterns):
+    """ Recursively find all files in a folder tree """
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if ".pyc" not in basename and "__pycache__" not in basename:
+                for pattern in patterns:
+                    if fnmatch.fnmatch(basename, pattern):
+                        filename = os.path.join(root, basename)
+                        yield filename
+
+
+# GUI applications require a different base on Windows
+iconFile = "openshot-qt"
+base = None
+src_files = []
+external_so_files = []
+exe_name = info.NAME
+
+# Copy QT translations to local folder (to be packaged)
+qt_local_path = os.path.join(PATH, "openshot_qt", "language")
+qt_system_path = QLibraryInfo.location(QLibraryInfo.TranslationsPath)
+if os.path.exists(qt_system_path):
+    for file in os.listdir(qt_system_path):
+        # Add QT translation files to include list
+        if (file.startswith("qt_") or file.startswith("qtbase_")) and file.endswith(".qm"):
+            src_files.append((os.path.join(qt_system_path, file), "language/{}".format(file)))
+
+# Copy git log files into src files (if found)
+for project in ["libopenshot-audio", "libopenshot", "openshot-qt"]:
+    git_log_path = os.path.join(PATH, "build", "install-x64", "share", "%s.log" % project)
+    if os.path.exists(git_log_path):
+        src_files.append((git_log_path, "settings/%s.log" % project))
+    else:
+        git_log_path = os.path.join(PATH, "build", "install-x86", "share", "%s.log" % project)
+        if os.path.exists(git_log_path):
+            src_files.append((git_log_path, "settings/%s.log" % project))
+
+if sys.platform == "win32":
+    # Define alternate terminal-based executable
+    extra_exe = {"base": None, "name": exe_name + "-cli.exe"}
+
+    # Standard graphical Win32 launcher
+    base = "Win32GUI"
+    build_exe_options["include_msvcr"] = True
+    exe_name += ".exe"
+
+    # Append Windows ICON file
+    iconFile += ".ico"
+
+    # Append some additional files for Windows (this is a debug launcher)
+    src_files.append((os.path.join(PATH, "installer", "launch-win.bat"), "launch-win.bat"))
+
+    # Add additional package
+    python_packages.append('idna')
+
+    # Manually add zmq dependency (windows does not freeze it correctly)
+    import zmq
+    python_packages.remove('zmq')
+    zmq_path = os.path.normpath(os.path.dirname(inspect.getfile(zmq)))
+    for filename in find_files(zmq_path, ["*"]):
+        src_files.append((filename, os.path.join("lib", "zmq", os.path.relpath(filename, start=zmq_path))))
+
+elif sys.platform == "linux":
+    # Find libopenshot.so path (GitLab copies artifacts into local build/install folder)
+    # libopenshot_path = os.path.join(PATH, "build", "install-x64", "lib")
+    # if not os.path.exists(libopenshot_path):
+    #     libopenshot_path = os.path.join(PATH, "build", "install-x86", "lib")
+    # if not os.path.exists(libopenshot_path):
+    #     # Try the archlib path
+    #     libopenshot_path = ARCHLIB
+    # if not os.path.exists(libopenshot_path):
+    #     # Default to user install path
+    #     libopenshot_path = "/usr/local/lib"
+
+    # # Find all related SO files
+    # for filename in find_files(libopenshot_path, ["*openshot*.so*"]):
+    #     if '_' in filename or filename.count(".") == 2:
+    #         external_so_files.append((filename, os.path.relpath(filename, start=libopenshot_path)))
+
+    # # Add libresvg (if found)
+    # resvg_path = "/usr/local/lib/libresvg.so"
+    # if os.path.exists(resvg_path):
+    #     external_so_files.append((resvg_path, os.path.basename(resvg_path)))
+    #
+    # Append Linux ICON file
+    iconFile += ".svg"
+    src_files.append((os.path.join(PATH, "xdg", iconFile), iconFile))
+
+    # Shorten name (since RPM can't have spaces)
+    info.PRODUCT_NAME = "openshot-qt"
+
+    # Add custom launcher script for frozen linux version
+    src_files.append((os.path.join(PATH, "installer", "launch-linux.sh"), "launch-linux.sh"))
+
+    # Get a list of all openshot.so dependencies (scan these libraries for their dependencies)
+    # pyqt5_mod_files = []
+    # from importlib import import_module
+    # for submod in ['Qt', 'QtWebKit', 'QtSvg', 'QtWebKitWidgets', 'QtWidgets', 'QtCore', 'QtGui', 'QtDBus']:
+    #     mod_name  = "PyQt5.{}".format(submod)
+    #     mod = import_module(mod_name)
+    #     pyqt5_mod_files.append(inspect.getfile(mod))
+    #
+    # lib_list = [os.path.join(libopenshot_path, "libopenshot.so"),
+    #             "/usr/local/lib/libresvg.so",
+    #             ARCHLIB + "qt5/plugins/platforms/libqxcb.so"
+    #             ] + pyqt5_mod_files
+
+elif sys.platform == "darwin":
+    # Copy Mac specific files that cx_Freeze misses
+    # JPEG library
+    for filename in find_files("/usr/local/Cellar/jpeg/8d/lib", ["libjpeg.8.dylib"]):
+        external_so_files.append((filename, filename.replace("/usr/local/Cellar/jpeg/8d/lib/", "")))
+
+    # Add libresvg (if found)
+    resvg_path = "/usr/local/lib/libresvg.dylib"
+    if os.path.exists(resvg_path):
+        external_so_files.append((resvg_path, resvg_path.replace("/usr/local/lib/", "")))
+
+    # Copy openshot.py Python bindings
+    src_files.append((os.path.join(PATH, "openshot.py"), "openshot.py"))
+    src_files.append((os.path.join(PATH, "installer", "launch-mac.sh"), "launch-mac.sh"))
+
+    # Append Mac ICON file
+    iconFile += ".hqx"
+    src_files.append((os.path.join(PATH, "xdg", iconFile), iconFile))
+
+# Append all source files
+# src_files.append((os.path.join(PATH, "installer", "qt.conf"), "qt.conf"))
+# for filename in find_files("openshot_qt", ["*"]):
+#     src_files.append((filename, filename.replace("openshot_qt/", "").replace("openshot_qt\\", "")))
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# build_exe_options["packages"] = python_packages
+# build_exe_options["include_files"] = src_files + external_so_files
+
+# Set options
+# build_options["build_exe"] = build_exe_options
+
+options = {
+    'build_exe': {
+        'includes': 'atexit',
+        'include_files': src_files,
+        # 'packages': python_packages,
+    }
+}
+
+# Define launcher executable to create
+exes = [
+    cx_Freeze.Executable(
+        "openshot_qt/__main__.py",
+        base=base,
+        icon=os.path.join(PATH, "xdg", iconFile),
+        shortcutName="%s" % info.PRODUCT_NAME,
+        shortcutDir="ProgramMenuFolder",
+        targetName=exe_name)
+]
+
+try:
+    # Include extra launcher configuration, if defined
+    exes.append(cx_Freeze.Executable(
+        "openshot_qt/__main__.py",
+        base=extra_exe['base'],
+        icon=os.path.join(PATH, "xdg", iconFile),
+        targetName=extra_exe['name']))
+except NameError:
+    pass
+
+# Create distutils setup object
+try:
+    cx_Freeze.setup(name=info.PRODUCT_NAME,
+          version=openshot_qt.version,
+          description=info.DESCRIPTION,
+          author=info.COMPANY_NAME,
+          options=options,
+          executables=exes)
+except Exception as ex:
+    log.error("Failed to create frozen executable", exc_info=1)
